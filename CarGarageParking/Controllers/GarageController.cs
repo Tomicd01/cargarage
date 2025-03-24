72bf915a5fc29585138448fe32e92f80b162ad55
Dusan Tomic <duletomic14901@gmail.com>
2025-03-24 14:22:02 +0100

    pagination added

--- CarGarageParking/Controllers/GarageController.cs
+++ CarGarageParking/Controllers/GarageController.cs
@@ -13,9 +13,10 @@ namespace CarGarageParking.Controllers
             _unitOfWork = unitOfWork;
         }
 
-        public IActionResult Index(string name, string location, int? AvailableSpots)
+        public IActionResult Index(string name, string location, int? AvailableSpots, int page = 1)
         {
             IEnumerable<Garage> garages = _unitOfWork.GarageService.GetAllgarages();
+
             if (name != null)
             {
                 garages = garages.Where(g => g.Name.Trim().ToLower() == name.Trim().ToLower());
@@ -29,6 +30,10 @@ namespace CarGarageParking.Controllers
                 Console.WriteLine(AvailableSpots);
                 garages = garages.Where(g => g.AvailableSpots >= AvailableSpots);
             }
+
+            var pageSize = 2;
+            garages = garages.Skip(pageSize * (page - 1)).Take(pageSize);
+
             return View(garages);
         }
 
@@ -55,6 +60,59 @@ namespace CarGarageParking.Controllers
                 return RedirectToAction("Index");
             }
             return View(garage);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            Garage garage = _unitOfWork.GarageService.GetGarageById(id);
+            if(garage == null)
+            {
+                return NotFound();
+            }
+            return View(garage);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Garage garage)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.GarageService.UpdateGarage(garage);
+                _unitOfWork.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(garage);    
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            Garage garage = _unitOfWork.GarageService.GetGarageById(id);
+            
+            if(garage == null)
+            {
+                return NotFound();
+            }
+            return View(garage);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int GarageId)
+        {
+            Garage garage = _unitOfWork.GarageService.GetGarageById(GarageId);
+
+            if(garage == null)
+            {
+                return NotFound();
+            }
+
+            _unitOfWork.GarageService.DeleteGarage(GarageId);
+            _unitOfWork.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
         }
 
     }
